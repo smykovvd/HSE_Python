406855396476e393e4b76517fa12a17ba906ba0a
Victor Smykov <vdsmykov@edu.hse.ru>
2024-09-18 14:42:27 +0300

    Fix style problems

--- 1module/1week/2task.py
+++ 1module/1week/2task.py
@@ -9,33 +9,34 @@ def longestPalindrome(self, s: str) -> str:
     ans1 = s[0]
     cur1 = 1
     for i in range(1, ln):
-        l = r = i
+        l1 = i
+        r1 = i
         while True:
-            if l > 0 and r < ln - 1 and s[l - 1] == s[r + 1]:
-                l -= 1
-                r += 1
+            if l1 > 0 and r1 < ln - 1 and s[l1 - 1] == s[r1 + 1]:
+                l1 -= 1
+                r1 += 1
             else:
                 break
-        if cur1 < r - l + 1:
-            cur1 = r - l + 1
-            ans1 = s[l : r + 1]
+        if cur1 < r1 - l1 + 1:
+            cur1 = r1 - l1 + 1
+            ans1 = s[l1: r1 + 1]
 
     ans2 = ""
     cur2 = 0
     for i in range(1, ln):
-        l = i - 1
-        r = i
-        if s[l] != s[r]:
+        l2 = i - 1
+        r2 = i
+        if s[l2] != s[r2]:
             continue
         while True:
-            if l > 0 and r < ln - 1 and s[l - 1] == s[r + 1]:
-                l -= 1
-                r += 1
+            if l2 > 0 and r2 < ln - 1 and s[l2 - 1] == s[r2 + 1]:
+                l2 -= 1
+                r2 += 1
             else:
                 break
-        if cur2 < r - l + 1:
-            cur2 = r - l + 1
-            ans2 = s[l : r + 1]
+        if cur2 < r2 - l2 + 1:
+            cur2 = r2 - l2 + 1
+            ans2 = s[l2: r2 + 1]
     return ans1 if cur1 > cur2 else ans2
 
 
